b528bfb55c88689dcfdf10b8c3c611ba99470784
Alejandro1988 <ale.aaaf@gmail.com>
2018-08-22 00:17:49 -0300

    Pom
    Cambio estuctura

--- src/main/java/com/pagina/SpringBootWebApplication.java
+++ src/main/java/com/pagina/SpringBootWebApplication.java
@@ -1,9 +1,9 @@
-package main.java.com.pagina;
+package com.pagina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringBootWebApplication extends SpringBootServletInitializer {
